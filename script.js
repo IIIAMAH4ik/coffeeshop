b7d474f0be8c430f57b45f7550ea5c6164ee02c2
IIIAMAH4ik <119852054+IIIAMAH4ik@users.noreply.github.com>
2023-09-13 18:21:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function showLayer(layerId) {
         selectedLayer.classList.add('active-layer');
     }
 }
-// Ваш JavaScript-код
+// script.js
 
 // Определение объекта корзины
 const cart = {
@@ -26,10 +26,38 @@ function addToCart(productName, productPrice) {
     const newItem = {
         name: productName,
         price: productPrice,
+        quantity: 1, // Начальное количество товара
     };
-    cart.items.push(newItem);
+    
+    // Проверка, есть ли уже такой товар в корзине
+    const existingItem = cart.items.find(item => item.name === productName);
+    
+    if (existingItem) {
+        existingItem.quantity += 1; // Увеличить количество товара
+    } else {
+        cart.items.push(newItem);
+    }
+    
     cart.total += productPrice;
     updateCartUI();
+}
+
+// Функция для удаления товара из корзины
+function removeFromCart(productName, productPrice) {
+    const existingItemIndex = cart.items.findIndex(item => item.name === productName);
+    
+    if (existingItemIndex !== -1) {
+        const existingItem = cart.items[existingItemIndex];
+        
+        if (existingItem.quantity > 1) {
+            existingItem.quantity -= 1; // Уменьшить количество товара
+        } else {
+            cart.items.splice(existingItemIndex, 1); // Удалить товар из корзины
+        }
+        
+        cart.total -= productPrice;
+        updateCartUI();
+    }
 }
 
 // Функция для обновления интерфейса корзины
@@ -45,7 +73,12 @@ function updateCartUI() {
             cartItem.classList.add('cart-item');
             cartItem.innerHTML = `
                 <p>${item.name}</p>
-                <p>$${item.price}</p>
+                <div>
+                    <button class="remove-button" data-name="${item.name}" data-price="${item.price}">-</button>
+                    <span>${item.quantity}</span>
+                    <button class="add-button" data-name="${item.name}" data-price="${item.price}">+</button>
+                </div>
+                <p>$${item.price * item.quantity}</p>
             `;
             cartContainer.appendChild(cartItem);
         });
@@ -55,5 +88,101 @@ function updateCartUI() {
         totalPrice.classList.add('total-price');
         totalPrice.textContent = `Общая стоимость: $${cart.total}`;
         cartContainer.appendChild(totalPrice);
+
+        // Добавить обработчики событий для кнопок "+", "-"
+        const addButtons = document.querySelectorAll('.add-button');
+        addButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                const productName = button.getAttribute('data-name');
+                const productPrice = parseFloat(button.getAttribute('data-price'));
+                addToCart(productName, productPrice);
+            });
+        });
+
+        const removeButtons = document.querySelectorAll('.remove-button');
+        removeButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                const productName = button.getAttribute('data-name');
+                const productPrice = parseFloat(button.getAttribute('data-price'));
+                removeFromCart(productName, productPrice);
+            });
+        });
     }
 }
+
+// Ваш JavaScript-код
+
+document.querySelectorAll('.add-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const productElement = button.closest('.product-item');
+        const productName = productElement.querySelector('h3').textContent;
+        const productPrice = parseFloat(productElement.querySelector('p').textContent.split('$')[1]);
+        const addToCartContainer = productElement.querySelector('.add-to-cart');
+        const quantitySpan = productElement.querySelector('.quantity');
+
+        // Проверка, есть ли уже такой товар в корзине
+        const existingItem = cart.items.find(item => item.name === productName);
+
+        if (existingItem) {
+            existingItem.quantity += 1; // Увеличить количество товара
+        } else {
+            cart.items.push({
+                name: productName,
+                price: productPrice,
+                quantity: 1, // Начальное количество товара
+            });
+        }
+
+        cart.total += productPrice;
+        updateCartUI();
+
+        // Показать кнопки "+", "-", и количество товаров
+        addToCartContainer.style.display = 'flex';
+        quantitySpan.textContent = existingItem ? existingItem.quantity : 1;
+
+        // Скрыть кнопку "Добавить"
+        button.style.display = 'none';
+    });
+});
+
+document.querySelectorAll('.increment-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const productElement = button.closest('.product-item');
+        const productName = productElement.querySelector('h3').textContent;
+        const existingItem = cart.items.find(item => item.name === productName);
+
+        if (existingItem) {
+            existingItem.quantity += 1; // Увеличить количество товара
+            cart.total += existingItem.price;
+            const quantitySpan = productElement.querySelector('.quantity');
+            quantitySpan.textContent = existingItem.quantity;
+            updateCartUI();
+        }
+    });
+});
+
+document.querySelectorAll('.decrement-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const productElement = button.closest('.product-item');
+        const productName = productElement.querySelector('h3').textContent;
+        const existingItem = cart.items.find(item => item.name === productName);
+
+        if (existingItem && existingItem.quantity > 0) {
+            existingItem.quantity -= 1; // Уменьшить количество товара
+            cart.total -= existingItem.price;
+            const quantitySpan = productElement.querySelector('.quantity');
+            quantitySpan.textContent = existingItem.quantity;
+            updateCartUI();
+
+            // Если количество стало нулевым, вернуть кнопку "Добавить"
+            if (existingItem.quantity === 0) {
+                const addButton = productElement.querySelector('.add-button');
+                addButton.style.display = 'block';
+                const addToCartContainer = productElement.querySelector('.add-to-cart');
+                addToCartContainer.style.display = 'none';
+            }
+        }
+    });
+});
+
+// Добавьте обработчики событий для кнопок "-" и "+" в корзине, если нужно
