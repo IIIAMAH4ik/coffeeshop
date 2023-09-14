9ba04d53f51b9594e1caa424fda1ddf510711ea4
IIIAMAH4ik <119852054+IIIAMAH4ik@users.noreply.github.com>
2023-09-14 11:11:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ function showOptions(button) {
     const product = button.closest('.product');
 
     // Находим все кнопки внутри контейнера
-    const optionButton1 = product.querySelector('.option-button1');
-    const optionButton2 = product.querySelector('.option-button2');
+    const optionButton1 = product.querySelector('.add-button,.option-button1');
+    const optionButton2 = product.querySelector('.add-button,.option-button2');
 
     // Скрываем кнопку "Добавить"
     buttons[0].style.display = 'none';
