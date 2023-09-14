e730769a9fe3f017b521241a5f1efb1c48bece3b
IIIAMAH4ik <119852054+IIIAMAH4ik@users.noreply.github.com>
2023-09-14 20:14:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,29 +10,35 @@ function showLayer(layerId) {
         selectedLayer.classList.add('active-layer');
     }
 }
-
 function showOptions(button) {
     // Находим родительский элемент (контейнер товара)
     const product = button.closest('.product');
-    // Находим все кнопки внутри контейнера
-    const addButton = product.querySelector('.add-button');
-    const optionButton1 = product.querySelector('.option-button1');
-    const optionButton2 = product.querySelector('.option-button2');
-
     // Находим элемент, который будет отображать количество товаров в корзине
     const itemCountElement = product.querySelector('.item-count');
-
     // Получаем текущее количество товаров
     let itemCount = parseInt(itemCountElement.textContent);
 
-    if (button === optionButton1) {
-        // Если нажата кнопка "-", уменьшаем количество товаров (не меньше 0)
+    // Находим кнопки "+-" внутри контейнера товара
+    const optionButton1 = product.querySelector('.option-button1');
+    const optionButton2 = product.querySelector('.option-button2');
+
+    if (button === optionButton1 && itemCount > 0) {
+        // Если нажата кнопка "-", уменьшаем количество товаров (но не меньше 0)
         itemCount = Math.max(itemCount - 1, 0);
     } else if (button === optionButton2) {
-        // Если нажата кнопка "+", увеличиваем количество товаров
-        itemCount += 1;
-    }
-
-    // Обновляем отображение количества товаров
+	@@ -47,9 +38,15 @@ function showOptions(button) {
     itemCountElement.textContent = itemCount;
+
+    // Показываем или скрываем кнопки "+-" в зависимости от значения itemCount
+    const addButton = product.querySelector('.add-button');
+
+    if (itemCount === 0) {
+        addButton.style.display = 'inline-block';
+        optionButton1.style.display = 'none';
+        optionButton2.style.display = 'none';
+    } else {
+        addButton.style.display = 'none';
+        optionButton1.style.display = 'inline-block';
+        optionButton2.style.display = 'inline-block';
+    }
 }
