723c5662dd5a74c53fe90e7f4f22a898c1a13f20
IIIAMAH4ik <119852054+IIIAMAH4ik@users.noreply.github.com>
2023-09-14 18:09:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,25 +16,36 @@ function showOptions(button) {
     // Находим родительский элемент (контейнер товара)
     const product = button.closest('.product');
 
-    // Находим все кнопки внутри контейнера
-    const addButton = product.querySelector('.add-button');
-    const optionButton1 = product.querySelector('.option-button1');
-    const optionButton2 = product.querySelector('.option-button2');
-
     // Находим элемент, который будет отображать количество товаров в корзине
     const itemCountElement = product.querySelector('.item-count');
 
     // Получаем текущее количество товаров
     let itemCount = parseInt(itemCountElement.textContent);
 
-    if (button === optionButton1) {
+    if (button === product.querySelector('.add-button')) {
+        // Если нажата кнопка "Добавить", увеличиваем количество товаров
+        itemCount += 1;
+    } else if (button === product.querySelector('.option-button1')) {
         // Если нажата кнопка "-", уменьшаем количество товаров (не меньше 0)
         itemCount = Math.max(itemCount - 1, 0);
-    } else if (button === optionButton2) {
+    } else if (button === product.querySelector('.option-button2')) {
         // Если нажата кнопка "+", увеличиваем количество товаров
         itemCount += 1;
     }
 
     // Обновляем отображение количества товаров
     itemCountElement.textContent = itemCount;
+    
+    // Показываем или скрываем кнопки "+" и "-" в зависимости от значения itemCount
+    const optionButton1 = product.querySelector('.option-button1');
+    const optionButton2 = product.querySelector('.option-button2');
+    
+    if (itemCount > 0) {
+        optionButton1.style.display = 'inline-block';
+        optionButton2.style.display = 'inline-block';
+    } else {
+        optionButton1.style.display = 'none';
+        optionButton2.style.display = 'none';
+    }
 }
+
