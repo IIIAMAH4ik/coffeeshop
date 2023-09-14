25608e86403a5caf4d25820ecd18b3bbc11ec521
IIIAMAH4ik <119852054+IIIAMAH4ik@users.noreply.github.com>
2023-09-14 09:54:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function showLayer(layerId) {
         selectedLayer.classList.add('active-layer');
     }
 }
+function showOptions(button) {
+    // Находим родительский элемент (контейнер товара)
+    const product = button.closest('.product');
+
+    // Находим все кнопки внутри контейнера
+    const buttons = product.querySelectorAll('.add-button, .option-button');
+
+    // Скрываем кнопку "Добавить"
+    buttons[0].style.display = 'none';
+
+    // Показываем кнопки опций
+    buttons[1].style.display = 'inline-block';
+    buttons[2].style.display = 'inline-block';
+}
